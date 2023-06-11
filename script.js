092564256af35c76a68b5accb84e72fef61feacb
KlemensNa <klemens.naue@gmail.com>
2023-06-11 18:11:09 +0200

    beforeDestroying

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
 let v = 0;
 
-
-function init(){
-    questionAmount();
-    renderQuestion(v);
+function init() {
 }
 
 
-function questionAmount(){
+function getReadyToStart(id){
+    deleteActiveCategory();
+    activateCategory(id);
+    createStartbutton(id);    
+}
+
+
+function deleteActiveCategory(){
+    document.getElementById("egypt").classList.remove('active');
+    document.getElementById("sports").classList.remove('active');
+    document.getElementById("it").classList.remove('active');
+    document.getElementById("movies").classList.remove('active');
+}
+
+
+function activateCategory(id){
+    let activeCategory = document.getElementById(`${id}`);
+    activeCategory.classList.add('active');
+}
+
+
+function createStartbutton(categorieID){
+    document.getElementById('startButton').innerHTML = /*html*/`
+        <button type="button" id="startBtn" class="btn" onclick="startQuiz(${categorieID})">Start Quiz</button>
+    `
+}
+
+
+function startQuiz(a){
+    toggleScreens();
+    questionAmount();
+    renderQuestion(a);
+
+}
+
+
+function toggleScreens(){
+    document.getElementById('startscreen').style = "display : none";
+    document.getElementById('menu').style = "display : none";
+    document.getElementById('playscreen').style = "";
+}
+
+
+function questionAmount() {
     let questionsAmount = document.getElementById('questionsAmount');
     let amount = questions.length;
 
@@ -17,17 +57,34 @@ function questionAmount(){
 }
 
 
-function renderQuestion(v){
-    let question = document.getElementById('question');
-    let q = questions[v];
-    question.innerHTML = /*html*/`
+function renderQuestion(v) {
+
+    if (v == questions.length) {
+        finishedQuiz(v);
+    } else {
+
+        let question = document.getElementById('question');
+        let q = questions[v];
+        question.innerHTML = /*html*/`
         ${q['question']}
-    `
-    renderAnswers(q);
+        `
+        renderAnswers(q);
+        actualQuestionNumber(v);
+        disableButton();
+    }
 }
 
 
-function renderAnswers(q){
+function actualQuestionNumber(v) {
+    let actualNumber = v + 1;
+
+    document.getElementById('actualNumber').innerHTML = /*html*/`
+        ${actualNumber}
+    `
+}
+
+
+function renderAnswers(q) {
     document.getElementById('answer1').innerHTML = q["answer1"];
     document.getElementById('answer2').innerHTML = q["answer2"];
     document.getElementById('answer3').innerHTML = q["answer3"];
@@ -35,29 +92,29 @@ function renderAnswers(q){
 }
 
 
-function logAnswer(id){
+function logAnswer(id) {
     createAnswerButton(id);
     lock(id);
 }
 
 
-function createAnswerButton(id){
+function createAnswerButton(id) {
     let button = document.getElementById('buttonAnswer');
 
-        button.innerHTML = /*html*/ `
+    button.innerHTML = /*html*/ `
             <button id="answerBtn" type="button" class="btn btn-warning" onclick="givenAnswer('${id}')">Antworten</button>
-        `   
+        `
 }
 
 
-function lock(id){
-    deleteLockedAnswer();    
-    let answerCard = document.getElementById(`${id}`);    
+function lock(id) {
+    deleteLockedAnswer();
+    let answerCard = document.getElementById(`${id}`);
     answerCard.classList.add('bg-locked');
 }
 
 
-function deleteLockedAnswer(){
+function deleteLockedAnswer() {
     document.getElementById(`answer1`).classList.remove('bg-locked');
     document.getElementById(`answer2`).classList.remove('bg-locked');
     document.getElementById(`answer3`).classList.remove('bg-locked');
@@ -65,38 +122,38 @@ function deleteLockedAnswer(){
 }
 
 
-function givenAnswer(id){
+function givenAnswer(id) {
     let rightAnswer = questions[v]["right_answer"];
     let givenAnswer = id.slice(-1);
     let correctAnswerFullName = `answer${rightAnswer}`;
     let button = document.getElementById('buttonAnswer');
 
-    if (rightAnswer == givenAnswer){
+    if (rightAnswer == givenAnswer) {
         document.getElementById(id).parentNode.classList.add('bg-success');
-        correct(button, id)
-    }else{
-        document.getElementById(id).parentNode.classList.add('bg-danger'); 
+        correct(button)
+    } else {
+        document.getElementById(id).parentNode.classList.add('bg-danger');
         document.getElementById(correctAnswerFullName).parentNode.classList.add('bg-success');
-        wrong(button, id);
+        wrong(button);
     }
 }
 
 
-function correct(button, id){    
-        button.innerHTML = /*html*/ `
+function correct(button) {
+    button.innerHTML = /*html*/ `
         <button id="nextQuestionButton" type="button" class="btn btn-success" onclick="nextQuestion()">Nächste Frage</button>
-    ` 
+    `
 }
 
 
-function wrong(button, id){        
-        button.innerHTML = /*html*/ `
+function wrong(button) {
+    button.innerHTML = /*html*/ `
         <button id="restartButton" type="button" class="btn btn-danger" onclick="restartQuiz()">Nochmal</button>
-    ` 
+    `
 }
 
 
-function nextQuestion(){
+function nextQuestion() {
     v++;
     renderQuestion(v);
     //in eigene Funtion und immer iweder benutzen --> deleteLocked Answer kann gelöscht werden
@@ -105,13 +162,50 @@ function nextQuestion(){
         document.getElementById(`answer${j}`).parentNode.classList.remove('bg-success');
         document.getElementById(`answer${j}`).classList.remove('bg-locked');
     }
-    
 }
 
 
-function restartQuiz(){
+function restartQuiz() {
     v = 0;
     renderQuestion(v);
+    for (let j = 1; j <= 4; j++) {
+        document.getElementById(`answer${j}`).parentNode.classList.remove('bg-danger');
+        document.getElementById(`answer${j}`).parentNode.classList.remove('bg-success');
+        document.getElementById(`answer${j}`).classList.remove('bg-locked');
+    }
+}
+
+
+function finishedQuiz() {
+    document.getElementById('playscreen').style = "display: none"
+    document.getElementById('endscreen').style = "";
+    document.getElementById('finalScore').innerHTML = `${(v)}`;
+
+    document.getElementById('maxScore').innerHTML = `${questions.length}`;
+}
+
+
+function startAgain(){
+    document.getElementById('playscreen').style = ""
+    document.getElementById('endscreen').style = "display: none";
+    restartQuiz();
+}
+
+
+function disableButton(){
+    document.getElementById('buttonAnswer').innerHTML = "";
+}
+
+
+
+
+function renderMenubar(){
+
+}
+
+
+function renderStartscreen(){
+
 }
 
 
--- scriptArrays.js
+++ scriptArrays.js
@@ -1,4 +1,4 @@
-let questions = [
+let egypt = [
     {
         "question": "Welcher Fluss fließt durch Ägypten?",
         "answer1": "Kongo",
@@ -13,40 +13,94 @@ let questions = [
         "answer2": "Lord",
         "answer3": "Netjer",
         "answer4": "Nesu",
-        "right_answer": 2
+        "right_answer": 1
+    }
+    // },
+    // {
+    //     "question": "Wie heißt die größte Pyramide auf dem Gizeh-Pateau?",
+    //     "answer1": "Mykerinos-Pyramide",
+    //     "answer2": "Djoser-Pyramide",
+    //     "answer3": "Cheops-Pyramide",
+    //     "answer4": "Chephren-Pyramide",
+    //     "right_answer": 3
+    // },
+    // {
+    //     "question": "Welcher Pharao schaffte für seine Amtszeit den Monotheismus ein?",
+    //     "answer1": "Nahmer",
+    //     "answer2": "Echnaton",
+    //     "answer3": "Tutanchamun",
+    //     "answer4": "Ramses II.",
+    //     "right_answer": 2
+    // },
+    // {
+    //     "question": "Wie heißt der größte jemals gefundene Tempel der Ägypter?",
+    //     "answer1": "Karnak",
+    //     "answer2": "Luxor",
+    //     "answer3": "Hermopolis",
+    //     "answer4": "Heliopolis",
+    //     "right_answer": 4
+    // },
+    // {
+    //     "question": "Wie heißen die langen viereckigen Säulen mit Pyramidenspitze?",
+    //     "answer1": "Obelisk",
+    //     "answer2": "Kapital",
+    //     "answer3": "Hypotstyl",
+    //     "answer4": "Memphis",
+    //     "right_answer": 1
+    // },
 
+]
+
+
+let sports = [
+    {
+        "question": "Wie heißt der Verein der das zweite mal hintereinander in der Relegation gescheitert ist?",
+        "answer1": "HSV",
+        "answer2": "Heidenheim",
+        "answer3": "Schalke",
+        "answer4": "Bayern",
+        "right_answer": 1
     },
     {
-        "question": "Welcher Fluss fließt durch Ägypten?",
-        "answer1": "Kongo",
-        "answer2": "Nil",
-        "answer3": "Amazonas",
-        "answer4": "Wolga",
-        "right_answer": 2
-    },
-    {
-        "question": "Welcher Fluss fließt durch Ägypten?",
-        "answer1": "Kongo",
-        "answer2": "Nil",
-        "answer3": "Amazonas",
-        "answer4": "Wolga",
-        "right_answer": 2
-    },
-    {
-        "question": "Welcher Fluss fließt durch Ägypten?",
-        "answer1": "Kongo",
-        "answer2": "Nil",
-        "answer3": "Amazonas",
-        "answer4": "Wolga",
-        "right_answer": 2
-    },
-    {
-        "question": "Welcher Fluss fließt durch Ägypten?",
-        "answer1": "Kongo",
-        "answer2": "Nil",
-        "answer3": "Amazonas",
-        "answer4": "Wolga",
-        "right_answer": 2
-    },
+        "question": "Wie hießen die alten Könige Ägypten?",
+        "answer1": "Pharao",
+        "answer2": "Lord",
+        "answer3": "Netjer",
+        "answer4": "Nesu",
+        "right_answer": 1
+    }
+    // },
+    // {
+    //     "question": "Wie heißt die größte Pyramide auf dem Gizeh-Pateau?",
+    //     "answer1": "Mykerinos-Pyramide",
+    //     "answer2": "Djoser-Pyramide",
+    //     "answer3": "Cheops-Pyramide",
+    //     "answer4": "Chephren-Pyramide",
+    //     "right_answer": 3
+    // },
+    // {
+    //     "question": "Welcher Pharao schaffte für seine Amtszeit den Monotheismus ein?",
+    //     "answer1": "Nahmer",
+    //     "answer2": "Echnaton",
+    //     "answer3": "Tutanchamun",
+    //     "answer4": "Ramses II.",
+    //     "right_answer": 2
+    // },
+    // {
+    //     "question": "Wie heißt der größte jemals gefundene Tempel der Ägypter?",
+    //     "answer1": "Karnak",
+    //     "answer2": "Luxor",
+    //     "answer3": "Hermopolis",
+    //     "answer4": "Heliopolis",
+    //     "right_answer": 4
+    // },
+    // {
+    //     "question": "Wie heißen die langen viereckigen Säulen mit Pyramidenspitze?",
+    //     "answer1": "Obelisk",
+    //     "answer2": "Kapital",
+    //     "answer3": "Hypotstyl",
+    //     "answer4": "Memphis",
+    //     "right_answer": 1
+    // },
 
 ]
